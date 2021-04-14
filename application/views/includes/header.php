6cfc3397892b95f05632b5497e791a0e9edc9d09
adriaauca <adriaac99@gmail.com>
2021-04-14 09:48:25 +0200

    solved favicon.io & confirm if nav_links are passed

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="#">
 
     <!-- CSS - Bootstrap 5.0 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
@@ -46,13 +47,21 @@
         if ($this->session->userdata('fk_role_id') == FALSE || ($this->session->userdata('fk_role_id') == TRUE && ROLE_ADMIN != $this->session->userdata('fk_role_id')))
 		{
         ?>
-            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <?php
+            if(isset($headerInfo['nav_links']))
+            {
+            ?>
+                <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <?php
+            }
+            ?>
+
             <input class="form-control form-control-dark w-100" type="text" placeholder="Search ..." aria-label="Search">
             <ul class="navbar-nav px-3">
                 <li class="nav-item text-nowrap">
-                    <a class="nav-link" href="#">
+                    <a class="nav-link" href="<?php echo base_url() ?>chart">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
                             <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
                         </svg>
@@ -110,36 +119,40 @@
     </header>
 
     <div class="container-fluid mt-5">
-        <div class="row">
-            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                <div class="position-sticky pt-3">
-                    <?php 
-                    if(isset($headerInfo['nav_links']))
-                    {
-                        foreach ($headerInfo['nav_links'] as $id => $links)
-                        {
-                            if ($id == 0)
+        <div class="row justify-content-center">
+            <?php 
+            if(isset($headerInfo['nav_links']))
+            {
+                ?>
+                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                    <div class="position-sticky pt-3">
+                        <?php 
+                            foreach ($headerInfo['nav_links'] as $id => $links)
                             {
-                            ?>
-                                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                                    <span><?php echo $links; ?></span>
-                                </h6>
-                                <ul class="nav flex-column mb-2">
-                            <?php
+                                if ($id == 0)
+                                {
+                                ?>
+                                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                                        <span><?php echo $links; ?></span>
+                                    </h6>
+                                    <ul class="nav flex-column mb-2">
+                                <?php
+                                }
+                                else
+                                {
+                                ?>
+                                    <li class="nav-item">
+                                        <a class="nav-link active" href="#<?php echo $links; ?>">
+                                            <?php echo $links; ?>
+                                        </a>
+                                    </li>
+                                <?php
+                                }
                             }
-                            else
-                            {
-                            ?>
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="#<?php echo $links; ?>">
-                                        <?php echo $links; ?>
-                                    </a>
-                                </li>
-                            <?php
-                            }
-                        }
-                        echo '</ul>';
-                    }
-                    ?>
-                </div>
-            </nav>
+                            echo '</ul>';
+                        ?>
+                    </div>
+                </nav>
+            <?php
+            }
+            ?>
