db1383a7a9eb38cb14a92e02a747bfe79f8e690b
adriaauca <adriaac99@gmail.com>
2021-04-16 15:58:15 +0200

    CRUD departments working

--- application/controllers/Departments.php
+++ application/controllers/Departments.php
@@ -18,48 +18,109 @@ class Departments extends BaseController {
 
 	public function index()
 	{
-	
-	}
+		$data = [];
 
-
-	public function create()
-	{
-
+		$data['departments'] = $this->departments_model->show_all();
+		$this->loadViews("departments", $this->global, $data, NULL);
 	}
 
 
 	public function store()
 	{
+		$name = $this->input->post('name');
+		$status = $this->input->post('status');
 
+		if (!isset($status))
+		{
+			$status = 0;
+		}
+
+		$this->form_validation->set_rules('name', 'Name', 'trim|required');
+
+		if($this->form_validation->run())
+		{
+			$data = array(
+				'name'				=>	$name,
+				'status'			=>	$status,
+			);
+
+			$id = $this->departments_model->store($data);
+
+			if ($id > 0)
+			{
+				$this->session->set_flashdata('success', 'Department added correctly!');
+			}
+			else
+			{
+				$this->session->set_flashdata('error', 'Department not added correctly!');
+			}
+		}
+
+		redirect('departments');
 	}
 
 
-	public function show($name)
-	{
-		$data = [];
+	public function show_all()
+    {
+        $data = [];
 
-		$name = str_replace('_', ' ', $name);
+		$data['data'] = $this->departments_model->show_all();
 
-		$data['product'] = $this->products_model->show($name);
-		//$data['query']=$this->db->last_query();
-		$this->loadViews("product", $this->global, $data, NULL);
-	}
-
-
-	public function edit()
-	{
-
-	}
+		echo json_encode($data);
+    }
 
 	
 	public function update()
 	{
+		$id = $this->input->post('id');
+		$name = $this->input->post('name');
+		$status = $this->input->post('status');
 
+		if (!isset($status))
+		{
+			$status = 0;
+		}
+
+		// CHECK IF DEPARTMENT EXIST
+		$exist = $this->departments_model->check_if_exist($id);
+
+		if (!empty($exist))
+		{
+			$updated = $this->departments_model->update($id, $name, $status);
+
+			if ($updated > 0)
+			{
+				$this->session->set_flashdata('success', 'Department updated correctly!');
+			}
+			else
+			{
+				$this->session->set_flashdata('error', 'Department not updated correctly!');
+			}
+		}
+
+		redirect('departments');
 	}
 
 
 	public function destroy()
 	{
+		$id = $this->input->post('id');
 
+		// CHECK IF DEPARTMENT EXIST
+		$exist = $this->departments_model->check_if_exist($id);
+
+		if (!empty($exist))
+		{
+			$deleted = $this->departments_model->destroy($id);
+
+			if ($deleted)
+			{
+				$this->session->set_flashdata('success', 'Department deleted correctly!');
+			}
+			else
+			{
+				$this->session->set_flashdata('error', 'Department not deleted correctly!');
+			}
+		}
 	}
 }
--- application/models/Departments_model.php
+++ application/models/Departments_model.php
@@ -2,9 +2,45 @@
 
 class Departments_model extends CI_Model
 {
-    public function get_departments()
+    public function store($data)
 	{
-        $this->db->select('id, name');
+        $this->db->insert('departments', $data);
+        return $this->db->insert_id();
+    }
+
+
+    public function show_all()
+	{
+        $this->db->select('*');
         return $this->db->get('departments')->result();
     }
+
+
+    public function update($id, $name, $status)
+	{
+        if ($name != NULL)
+        {
+            $this->db->set('name', $name);
+        }
+        $this->db->set('status', $status);
+        $this->db->where('id', $id);
+        $this->db->update('departments');
+
+		return $this->db->affected_rows();
+	}
+
+
+    public function destroy($id)
+	{
+		return $this->db->delete('departments', array('id' => $id));
+	}
+
+
+    public function check_if_exist($id)
+	{
+        $this->db->select('*');
+        $this->db->from('departments');
+        $this->db->where('id', $id);
+        return $this->db->get()->row();
+	}
 }
